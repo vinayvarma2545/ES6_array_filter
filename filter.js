a0f408564e685feb8e9a771ffdb2565cb7bc8287
vinay <vinayvarma2545@gmail.com>
2022-09-19 21:41:14 +0530

    filter

---
+++ filter.js
@@ -0,0 +1,44 @@
+// let employees = [
+//     {
+//      id: 1,
+//      name: "joe"
+//     },
+//     {
+//      id: 19,
+//      name: "john"
+//     },
+//     {
+//      id: 20,
+//      name: "james"
+//     },
+//     {
+//      id: 25,
+//      name: "jack"
+//     },
+//     {
+//      id: 10,
+//      name: "joseph"
+//     },
+//     {
+//      id: "not a number",
+//      name: "jimmy"
+//     },
+//     {
+//      id: null,
+//      name: "jeff"
+//     },
+//    ]
+   
+//    let result = employees.filter(x => x.id > 10);
+
+   
+//    console.log(result);
+   
+
+
+   let box1 = [movie={hero:"ram",song:"vana"},{hero:"krish",song:"hair"},
+               hospital={area:"nagar",doc:"patel"},{area:"chowk",doc:"varma"},{area:"bhag",doc:"sharma"},
+               office={id:121,des:"ceo"},{id:200,des:"staff"},{id:99,des:"manager"}]
+
+   let result = box1.filter(x => x.id<150);
+   console.log(result)           
